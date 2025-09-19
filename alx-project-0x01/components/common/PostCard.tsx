0963283fbc7c4fc1479f3bf3069633755ab3f71a
Elias Derbew <eliasderbew1@gmail.com>
2025-09-19 21:10:45 +0300

    Implement Posts Card Components

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,9 +1,18 @@
-const PostCard: React.FC = () => {
+import { PostProps } from "@/interfaces";
+
+const PostCard: React.FC<PostProps> = ({ title, body, userId, id }) => {
   return (
     <div>
-      <h1>this is the post card component</h1>
+      <div>
+        <h2>{title}</h2>
+        <p>{body}</p>
+        <div>
+          <span>User ID: {userId}</span>
+          <span>Post ID: {id}</span>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PostCard
+export default PostCard;
